0f72c04aed9ae272f68af619f5bb83a9f37f2036
selfdual-brain <wojciech.zaborowski@gmail.com>
2022-06-17 00:18:36 +0200

    work in progress

--- src/main/scala/io/spacemesh/platform/ParsedTxPayload.scala
+++ src/main/scala/io/spacemesh/platform/ParsedTxPayload.scala
@@ -1,11 +1,11 @@
 package io.spacemesh.platform
 
-abstract class ParsedTxPayload{
-  def methodArguments: Array[Byte]
-  def nonce: Nonce
-  def minLayer: Long
-  def maxLayer: Long
-  def maxGas: Gas
-  def gasPrice: Ether
-}
+case class ParsedTxPayload(
+  methodArgumentsBlob: Array[Byte],
+  nonce: Nonce,
+  minLayer: Long,
+  maxLayer: Long,
+  maxGas: Gas,
+  gasPrice: Ether,
+)
                      
